f799b326408f9d3a51a05fd589bfa4ccd65067c5
hieu98 <hieucot1998@gmail.com>
2020-09-06 20:02:56 +0700

    fix

--- app/src/main/java/com/example/noteapp/model/User.kt
+++ app/src/main/java/com/example/noteapp/model/User.kt
@@ -1,5 +1,7 @@
 package com.example.noteapp.model
 
+import com.google.firebase.database.IgnoreExtraProperties
 import java.io.Serializable
 
-data class User ( val username:String, val email:String):Serializable
+@IgnoreExtraProperties
+data class User ( val userid:String, val username:String, val email:String, val phone:String):Serializable
