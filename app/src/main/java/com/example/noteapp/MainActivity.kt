3d7f86dfa4487cf3f5c64b9299fcfef68635215c
hieu98 <hieucot1998@gmail.com>
2020-09-09 04:19:45 +0700

    bottom navigation

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.noteapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.luseen.spacenavigation.SpaceItem
+import com.luseen.spacenavigation.SpaceNavigationView
+import com.luseen.spacenavigation.SpaceOnClickListener
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nav : SpaceNavigationView = space
+        var a= false
+        hidebutton(a)
+        nav.initWithSaveInstanceState(savedInstanceState)
+        nav.addSpaceItem(SpaceItem("HOME", R.drawable.ic_baseline_home_24))
+        nav.addSpaceItem(SpaceItem("SEARCH", R.drawable.ic_baseline_person_24))
+        nav.setSpaceOnClickListener(object : SpaceOnClickListener {
+            override fun onCentreButtonClick() {
+//                Toast.makeText(this@MainActivity, "onCentreButtonClick", Toast.LENGTH_SHORT).show()
+                nav.setCentreButtonSelectable(true)
+                if(a==false){
+                    hidebutton(true)
+                    a = true
+                }else {
+                    hidebutton(false)
+                    a=false
+                }
+            }
+
+            override fun onItemClick(itemIndex: Int, itemName: String) {
+//                Toast.makeText(this@MainActivity, "$itemIndex $itemName", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemReselected(itemIndex: Int, itemName: String) {
+//                Toast.makeText(this@MainActivity, "$itemIndex $itemName", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
+    fun hidebutton(a : Boolean) {
+        if (a==true){
+            btn1.visibility= View.VISIBLE
+            btn2.visibility= View.VISIBLE
+
+        }else {
+            btn1.visibility = View.INVISIBLE
+            btn2.visibility = View.INVISIBLE
+
+        }
+    }
+
 }
