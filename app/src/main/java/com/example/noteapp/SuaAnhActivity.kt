e5a58d0bde62442b3c91f1ddebae1aba2c990d8d
anphapk55 <anphapk55@gmail.com>
2020-10-22 23:21:57 +0700

    forth commit

--- app/src/main/java/com/example/noteapp/SuaAnhActivity.kt
+++ app/src/main/java/com/example/noteapp/SuaAnhActivity.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.net.Uri
 import android.os.Build
@@ -15,7 +17,6 @@ import android.text.TextUtils
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
@@ -39,10 +40,8 @@ import com.zomato.photofilters.imageprocessors.subfilters.ContrastSubFilter
 import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter
 import kotlinx.android.synthetic.main.activity_suaanh.*
 import kotlinx.android.synthetic.main.content_main.*
-import java.util.*
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
 import java.lang.Math.sqrt
+import java.util.*
 
 class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImageFragmentListener {
     val SELECT_GALLERY_PERMISSION = 1000
@@ -72,7 +71,6 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
     private var lastAcceleration = 0f
 
     private val SHAKE_SLOP_TIME_MS = 100000
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
 
     object Main {
         val IMAGE_NAME = "flash.jpg"
@@ -112,26 +110,26 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
         editImageFragment = EditImageFragment()
         editImageFragment.setListener(this)
 
-        adapter.addFragment(filterListFragment,"FILTERS")
-        adapter.addFragment(editImageFragment,"EDIT")
+        adapter.addFragment(filterListFragment, "FILTERS")
+        adapter.addFragment(editImageFragment, "EDIT")
 
         viewPager?.adapter = adapter
 
     }
 
     private fun loadImage() {
-        originalImage = BitmapUtils.getBitmapFromAssets(this,Main.IMAGE_NAME,300,300)
-        filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888,true)
-        finalImage  = originalImage!!.copy(Bitmap.Config.ARGB_8888,true)
+        originalImage = BitmapUtils.getBitmapFromAssets(this, Main.IMAGE_NAME, 300, 300)
+        filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true)
+        finalImage  = originalImage!!.copy(Bitmap.Config.ARGB_8888, true)
         image_preview.setImageBitmap(originalImage)
     }
 
     override fun onFilterSelected(filter: Filter) {
         resetControl()
-        filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888,true)
-        Log.e("halo","ddaay")
+        filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true)
+        Log.e("halo", "ddaay")
         image_preview.setImageBitmap(filter.processFilter(filteredImage))
-        finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888,true)
+        finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888, true)
     }
 
     private fun resetControl() {
@@ -144,23 +142,44 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
     override fun onBrightnessChanged(brightness: Int) {
         brightnessFinal = brightness
         val myFilter = Filter()
-        Log.e("bri","ok")
+        Log.e("bri", "ok")
         myFilter.addSubFilter(BrightnessSubFilter(brightness))
-        image_preview.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888,true)))
+        image_preview.setImageBitmap(
+            myFilter.processFilter(
+                finalImage.copy(
+                    Bitmap.Config.ARGB_8888,
+                    true
+                )
+            )
+        )
     }
 
     override fun onSaturationChanged(saturation: Float) {
         saturationFinal = saturation
         val myFilter = Filter()
         myFilter.addSubFilter(SaturationSubfilter(saturation))
-        image_preview.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888,true)))
+        image_preview.setImageBitmap(
+            myFilter.processFilter(
+                finalImage.copy(
+                    Bitmap.Config.ARGB_8888,
+                    true
+                )
+            )
+        )
     }
 
     override fun onConstrantChanged(constrant: Float) {
         constrantFinal = constrant
         val myFilter = Filter()
         myFilter.addSubFilter(ContrastSubFilter(constrant))
-        image_preview.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888,true)))
+        image_preview.setImageBitmap(
+            myFilter.processFilter(
+                finalImage.copy(
+                    Bitmap.Config.ARGB_8888,
+                    true
+                )
+            )
+        )
     }
 
     override fun onEditStarted() {
@@ -168,7 +187,7 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
     }
 
     override fun onEditCompleted() {
-        val bitmap = filteredImage.copy(Bitmap.Config.ARGB_8888,true)
+        val bitmap = filteredImage.copy(Bitmap.Config.ARGB_8888, true)
         val myFilter = Filter()
         myFilter.addSubFilter(BrightnessSubFilter(brightnessFinal))
         myFilter.addSubFilter(SaturationSubfilter(saturationFinal))
@@ -177,7 +196,7 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
@@ -212,17 +231,21 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
             )
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                    if (p0!!.areAllPermissionsGranted()){
+                    if (p0!!.areAllPermissionsGranted()) {
                         val value = ContentValues()
-                        value.put(MediaStore.Images.Media.TITLE,"New Picture")
-                        value.put(MediaStore.Images.Media.DESCRIPTION,"From Camera")
-                        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,value)
+                        value.put(MediaStore.Images.Media.TITLE, "New Picture")
+                        value.put(MediaStore.Images.Media.DESCRIPTION, "From Camera")
+                        imageUri = contentResolver.insert(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            value
+                        )
 
                         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
-                        startActivityForResult(cameraIntent,CAMERA_REQUEST)
-                    }else
-                        Toast.makeText(applicationContext,"Permission denied",Toast.LENGTH_SHORT).show()
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST)
+                    } else
+                        Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT)
+                            .show()
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
@@ -243,22 +266,32 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
             )
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                    if (p0!!.areAllPermissionsGranted()){
-                        val path = BitmapUtils.insertImage(contentResolver,finalImage,
-                            System.currentTimeMillis().toString() + "_profile.jpg","")
-                        if (!TextUtils.isEmpty(path)){
-                            val snackBar = Snackbar.make(coordinator,"Image save to gallery",Snackbar.LENGTH_LONG)
+                    if (p0!!.areAllPermissionsGranted()) {
+                        val path = BitmapUtils.insertImage(
+                            contentResolver, finalImage,
+                            System.currentTimeMillis().toString() + "_profile.jpg", ""
+                        )
+                        if (!TextUtils.isEmpty(path)) {
+                            val snackBar = Snackbar.make(
+                                coordinator,
+                                "Image save to gallery",
+                                Snackbar.LENGTH_LONG
+                            )
                                 .setAction("OPEN") {
                                     openImage(path)
                                 }
                             snackBar.show()
-                        }
-                        else {
-                            val snackBar = Snackbar.make(coordinator,"Unable to save image",Snackbar.LENGTH_LONG)
+                        } else {
+                            val snackBar = Snackbar.make(
+                                coordinator,
+                                "Unable to save image",
+                                Snackbar.LENGTH_LONG
+                            )
                             snackBar.show()
                         }
-                    }else
-                        Toast.makeText(applicationContext,"Permission denied",Toast.LENGTH_SHORT).show()
+                    } else
+                        Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT)
+                            .show()
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
@@ -271,10 +304,10 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
             }).check()
     }
 
-    private fun openImage(path:String?) {
+    private fun openImage(path: String?) {
         val intent = Intent()
         intent.action = Intent.ACTION_VIEW
-        intent.setDataAndType(Uri.parse(path),"image/*")
+        intent.setDataAndType(Uri.parse(path), "image/*")
         startActivity(intent)
     }
 
@@ -286,12 +319,13 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
             )
             .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                    if (p0!!.areAllPermissionsGranted()){
+                    if (p0!!.areAllPermissionsGranted()) {
                         val intent = Intent(Intent.ACTION_PICK)
                         intent.type = "image/*"
-                        startActivityForResult(intent,SELECT_GALLERY_PERMISSION)
-                    }else
-                        Toast.makeText(applicationContext,"Permission denied",Toast.LENGTH_SHORT).show()
+                        startActivityForResult(intent, SELECT_GALLERY_PERMISSION)
+                    } else
+                        Toast.makeText(applicationContext, "Permission denied", Toast.LENGTH_SHORT)
+                            .show()
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
@@ -307,15 +341,15 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == SELECT_GALLERY_PERMISSION){
-            val bitmap = BitmapUtils.getBitmapFromGallery(this,data?.data!!,800,800)
+            val bitmap = BitmapUtils.getBitmapFromGallery(this, data?.data!!, 800, 800)
 
             originalImage!!.recycle()
             filteredImage.recycle()
             finalImage.recycle()
 
-            originalImage= bitmap?.copy(Bitmap.Config.ARGB_8888,true)
-            filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888,true)
-            finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888,true)
+            originalImage= bitmap?.copy(Bitmap.Config.ARGB_8888, true)
+            filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true)
+            finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888, true)
 
             image_preview.setImageBitmap(originalImage)
             bitmap?.recycle()
@@ -323,22 +357,20 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
             filterListFragment.displayImage(originalImage)
 
         }else if (resultCode == Activity.RESULT_OK && requestCode == CAMERA_REQUEST){
-            val bitmap = BitmapUtils.getBitmapFromGallery(this,imageUri!!,800,800)
+            val bitmap = BitmapUtils.getBitmapFromGallery(this, imageUri!!, 800, 800)
 
             originalImage!!.recycle()
             filteredImage.recycle()
             finalImage.recycle()
 
-            originalImage= bitmap?.copy(Bitmap.Config.ARGB_8888,true)
-            filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888,true)
-            finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888,true)
+            originalImage= bitmap?.copy(Bitmap.Config.ARGB_8888, true)
+            filteredImage = originalImage!!.copy(Bitmap.Config.ARGB_8888, true)
+            finalImage = filteredImage.copy(Bitmap.Config.ARGB_8888, true)
 
             image_preview.setImageBitmap(originalImage)
             bitmap?.recycle()
             filterListFragment = FilterListFragment.getInstance(originalImage)
             filterListFragment.setListener(this)
-
-
         }
     }
 
@@ -363,11 +395,11 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
                 }
                 mShakeTimestamp = now
 
-                if (Round(x,4)< -30.0000){
+                if (Round(x, 4)< -30.0000){
                     val imageView = findViewById<ImageView>(R.id.image_preview)
                     imageView.rotation = imageView.rotation - 90
                     Toast.makeText(applicationContext, "Rotate Right", Toast.LENGTH_SHORT).show()
-                }else if (Round(x,4)>30.0000) {
+                }else if (Round(x, 4)>30.0000) {
                     val imageView = findViewById<ImageView>(R.id.image_preview)
                     imageView.rotation = imageView.rotation + 90
                     Toast.makeText(applicationContext, "Rotate Left", Toast.LENGTH_SHORT).show()
@@ -391,6 +423,8 @@ class SuaAnhActivity : AppCompatActivity(), FilterListFragmentListener, EditImag
         mSensorManager?.unregisterListener(sensorListener)
         super.onPause()
     }
+
+    //Làm tròn x
     fun Round(Rval: Float, Rpl: Int): Float {
         var Rval = Rval
         val p = Math.pow(10.0, Rpl.toDouble()).toFloat()
